8fe86789e2c42243bb2d563d21fbc8d6c53c5484
Aroobha Amjad <132396789+aroobhaamjad@users.noreply.github.com>
2026-04-25 16:07:17 -0400

    Enable CORS for Flask application

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ Original file is located at
 """
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
 model = joblib.load('vino_veritas_model.pkl')
 scaler = joblib.load('scaler.pkl')
